0bd27efdd92cb5ec2ad32f4a0c99cb75f1eaa312
Avery.Davenport <averymdavenport@gmail.com>
2021-06-29 15:42:08 -0600

    8

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const path = require('path');
 const app = express();
 var Rollbar = require("rollbar");
@@ -8,18 +9,39 @@ var rollbar = new Rollbar({
   captureUnhandledRejections: true
 });
 
+app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-function students(){
-    
-}
+let animals = [];
 
-app.get('/', function(req, res){
-    rollbar.log("Hello Avery");
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+rollbar.log('Hello Avery');
+
+app.post('/api/animals', (req,res) => {
+    let { animal } = req.body
+
+    const index = animals.findIndex((animalName) => {
+        animalName === animal
+    })
+
+    try {
+        if (index === -1 && animal !== ''){
+            animals.push(animal)
+            rollbar.log('animal added successfully', {author: 'avery', type: 'manual'})
+            res.status(200).send(animals)
+        } else if (animal === '') {
+            rollbar.error('no animal provided')
+            res.status(400).send('must provide an animal')
+        } else {
+            rollbar.error('animal already exists')
+            res.status(400).send('that animal already exists')
+        }
+    } catch (err) {
+        rollbar.error(err)
+    }
 });
 
 
-
 const port = process.env.PORT || 4000;
+app.use(rollbar.errorHandler());
 app.listen(port, function(){console.log(`server working on ${port}`)})
