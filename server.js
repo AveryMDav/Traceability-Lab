0362b78127ee04c14d261bef8fe1316b5d396fe7
Avery.Davenport <averymdavenport@gmail.com>
2021-06-29 13:55:22 -0600

    1

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '7d37c89ddd2047fab01e3d1c7ef5b293',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
 
 app.use(express.json());
 
 app.get('/', function(req, res){
+    rollbar.log("Hello world!");
+    
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
